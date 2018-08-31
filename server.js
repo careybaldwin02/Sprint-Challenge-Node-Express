19f7f873a8aa62155e7ae49c5a51ffdef56da310
Carey <careycbaldwin@gmail.com>
2018-08-31 12:20:53 -0400

    completed get request for actions

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const server = express();
 
 //connect to the database
 const projects = require('./data/helpers/projectModel.js');
+const actions = require('./data/helpers/actionModel.js');
+const mappers = require('./data/helpers/mappers.js');
 
 //allow parsing of data objects
 server.use(express.json())
@@ -19,11 +21,24 @@ server.get('/', (req,res) => {
 
 server.listen(9000, () => console.log('\n== API can be found at http://localhost:9000/ ==\n'))
 
-//GET request
+//GET request for projects
 server.get('/api/projects', (req, res) => {
     projects.get()
     .then(projects => {
         res.status(200).json(projects);
+    })
+    .catch(err => {
+        console.error('error',err);
+        res.status(500).json({message: 'Error getting data'})
+    });
+});
+
+
+//GET request for actions
+server.get('/api/actions', (req, res) => {
+    actions.get()
+    .then(actions => {
+        res.status(200).json(actions);
     })
     .catch(err => {
         console.error('error',err);
