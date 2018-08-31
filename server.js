197e089c4a7421443bbcca292b9e77d3aa12877c
Carey <careycbaldwin@gmail.com>
2018-08-31 12:34:37 -0400

    individual GET requests for projects and actions

--- server.js
+++ server.js
@@ -21,7 +21,10 @@ server.get('/', (req,res) => {
 
 server.listen(9000, () => console.log('\n== API can be found at http://localhost:9000/ ==\n'))
 
-//GET request for projects
+//*************PROJECTS***************
+//Project fields: id, name (string), descriptiion (string), completed (T/F)
+
+//GET request for all projects
 server.get('/api/projects', (req, res) => {
     projects.get()
     .then(projects => {
@@ -34,7 +37,33 @@ server.get('/api/projects', (req, res) => {
 });
 
 
-//GET request for actions
+//individual GET request for project
+server.get('/api/projects/:id', (req,res) => {
+    const id = req.params.id;
+    projects
+        .get(id)
+        .then(project => {
+            if(project.length === 0){
+                res.status(404).json({message: 'No project corresponding to that identifier'});
+                return;
+            }
+            res.json(project);
+        })
+        .catch(error => {
+            res.status(500).json({message: 'Error looking for project'});
+        });
+});
+
+
+//POST request for project
+
+//DELETE request for project
+
+//PUT request for project
+
+
+//*************ACTIONs***************
+//GET request for all actions
 server.get('/api/actions', (req, res) => {
     actions.get()
     .then(actions => {
@@ -45,3 +74,27 @@ server.get('/api/actions', (req, res) => {
         res.status(500).json({message: 'Error getting data'})
     });
 });
+
+//individual GET request for action
+server.get('/api/actions/:id', (req,res) => {
+    const id = req.params.id;
+    actions
+        .get(id)
+        .then(action => {
+            if(action.length === 0){
+                res.status(404).json({message: 'No actions corresponding to that identifier'});
+                return;
+            }
+            res.json(action);
+        })
+        .catch(error => {
+            res.status(500).json({message: 'Error looking for action'});
+        });
+});
+
+
+//POST request for action
+
+//DELETE request for action
+
+//PUT request for action
