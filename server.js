4455f494711bf61105bb9ec494a958563ec27406
Carey <careycbaldwin@gmail.com>
2018-08-31 12:17:38 -0400

    completed get request for projects

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+//*************IMPORTS****************/
 const express = require('express');
+
+//creates an express application using the express module
 const server = express();
+
+//connect to the database
+const projects = require('./data/helpers/projectModel.js');
+
+//allow parsing of data objects
+server.use(express.json())
+
+//*************ROUTES HERE***************
 
 //Route to initiate server
 server.get('/', (req,res) => {
@@ -7,3 +18,15 @@ server.get('/', (req,res) => {
 });
 
 server.listen(9000, () => console.log('\n== API can be found at http://localhost:9000/ ==\n'))
+
+//GET request
+server.get('/api/projects', (req, res) => {
+    projects.get()
+    .then(projects => {
+        res.status(200).json(projects);
+    })
+    .catch(err => {
+        console.error('error',err);
+        res.status(500).json({message: 'Error getting data'})
+    });
+});
